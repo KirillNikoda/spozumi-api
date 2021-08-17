717f86bf89e3838d327821a18ad07f5300fea698
KirillNikoda <nikodakirill386@gmail.com>
2021-08-17 20:43:25 +0300

    added guards

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -6,9 +6,13 @@ import {
   HttpCode,
   Param,
   ParseIntPipe,
-  Put
+  Put,
+  Request,
+  UnauthorizedException,
+  UseGuards
 } from '@nestjs/common';
 import { UpdateUserDto } from 'src/dtos/updateUser.dto';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
 import { UserService } from 'src/services/user/user.service';
 
 @Controller('users')
@@ -20,17 +24,21 @@ export class UserController {
     return this.userService.getAllUsers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  public async getUser(@Param('id', ParseIntPipe) id: number) {
+  public async getUser(@Param('id', ParseIntPipe) id: number, @Request() req) {
+    console.log('hello', req.user);
     return this.userService.findUser(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   @HttpCode(204)
   public async deleteUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.deleteUser(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   public async updateUser(
     @Param('id', ParseIntPipe) id: number,
