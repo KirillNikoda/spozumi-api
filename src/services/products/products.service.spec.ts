1b4c8e60b094c5db2eeec288bb9ebbec1435a902
KirillNikoda <nikodakirill386@gmail.com>
2021-08-18 20:40:13 +0300

    wrote tests for ProductsController & ProductsService

--- src/services/products/products.service.spec.ts
+++ src/services/products/products.service.spec.ts
@@ -1,18 +1,115 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateProductDto } from 'src/dtos/createProduct.dto';
+import { UpdateProductDto } from 'src/dtos/updateProduct.dto';
+import { Brand } from 'src/entities/brand.entity';
+import { Category } from 'src/entities/category.entity';
+import { Product } from 'src/entities/product.entity';
 import { ProductsService } from './products.service';
 
-describe('ProductsService', () => {
+describe('Test ProductsService', () => {
   let service: ProductsService;
+  let mockedProductRepository = {
+    findOne: jest.fn((id: number) =>
+      typeof id === 'number' ? { id, name: 'jacket' } : null
+    ),
+    find: jest.fn(() => [
+      { id: 1, name: 'jacket' },
+      { id: 2, name: 'coat' }
+    ]),
+    update: jest.fn((id: number, dto: UpdateProductDto) => ({ id, ...dto })),
+    save: jest.fn((dto: CreateProductDto) => ({ id: 1, ...dto })),
+    delete: jest.fn((id: number) => ({ id, success: true }))
+  };
+
+  let mockedCategoryRepository = {
+    findOne: jest.fn(() => new Category())
+  };
+
+  let mockedBrandRepository = {
+    findOne: jest.fn(() => new Brand())
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockedProductRepository
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: mockedCategoryRepository
+        },
+        {
+          provide: getRepositoryToken(Brand),
+          useValue: mockedBrandRepository
+        }
+      ]
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should get product by id and return it', async () => {
+    const id = 1;
+    const product = await service.getProduct(id);
+
+    expect(product.id).toEqual(id);
+    expect(product.name).toEqual('jacket');
+    expect(mockedProductRepository.findOne).toBeCalled();
+    expect(mockedProductRepository.findOne).toBeCalledTimes(1);
+  });
+
+  it('Should get all products and return them', async () => {
+    const products = await service.getProducts();
+
+    expect(products).toHaveLength(2);
+    expect(products[0].name).toEqual('jacket');
+    expect(mockedProductRepository.find).toBeCalled();
+    expect(mockedProductRepository.find).toBeCalledTimes(1);
+  });
+
+  it('Should delete product by id', async () => {
+    const id = 1;
+
+    const result: any = await service.deleteProduct(id);
+
+    expect(result.success).toBeTruthy();
+    expect(result.id).toEqual(id);
+    expect(mockedProductRepository.delete).toBeCalled();
+    expect(mockedProductRepository.delete).toBeCalledTimes(1);
+  });
+
+  it('Should update product by id', async () => {
+    const id = 1;
+    const dto = {
+      name: 'coat'
+    } as UpdateProductDto;
+
+    const result: any = await service.updateProduct(id, dto);
+
+    expect(result.id).toEqual(id);
+    expect(result.name).toEqual(dto.name);
+    expect(mockedProductRepository.update).toBeCalled();
+    expect(mockedProductRepository.update).toBeCalledTimes(1);
+  });
+
+  it('Should create product', async () => {
+    const dto = {
+      name: 'coat'
+    } as CreateProductDto;
+
+    const createdProduct = await service.createProduct(dto);
+
+    expect(createdProduct.name).toEqual(dto.name);
+    expect(createdProduct.id).toEqual(1);
+    expect(mockedProductRepository.save).toBeCalled();
+    expect(mockedProductRepository.save).toBeCalledTimes(1);
+    expect(mockedCategoryRepository.findOne).toBeCalled();
+    expect(mockedCategoryRepository.findOne).toBeCalledTimes(1);
+    expect(mockedBrandRepository.findOne).toBeCalled();
+    expect(mockedBrandRepository.findOne).toBeCalledTimes(1);
   });
 });
--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -7,13 +7,20 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from 'src/dtos/createProduct.dto';
 import { UpdateProductDto } from 'src/dtos/updateProduct.dto';
+import { Brand } from 'src/entities/brand.entity';
+import { Category } from 'src/entities/category.entity';
 import { Product } from 'src/entities/product.entity';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
   constructor(
-    @InjectRepository(Product) private productRepository: Repository<Product>
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+    @InjectRepository(Brand)
+    private brandRepository: Repository<Brand>
   ) {}
 
   public async getProducts(): Promise<Product[]> {
@@ -48,6 +55,18 @@ export class ProductsService {
     if (productExists) {
       throw new BadRequestException('Product with that name already exists');
     }
+
+    const [brand, category] = await Promise.all([
+      this.brandRepository.findOne({
+        brandName: String(product.brand)
+      }),
+      this.categoryRepository.findOne({
+        categoryName: String(product.category)
+      })
+    ]);
+
+    product.category = category;
+    product.brand = brand;
 
     const createdProduct = await this.productRepository.save(product);
 
