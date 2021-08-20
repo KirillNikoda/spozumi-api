4cbd246a3f52b1bc882d8f3a73efd18bc9ed2744
KirillNikoda <nikodakirill386@gmail.com>
2021-08-20 17:36:33 +0300

    some changes

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from 'src/controllers/products/products.controller';
+import { Brand } from 'src/entities/brand.entity';
+import { Category } from 'src/entities/category.entity';
 import { Product } from 'src/entities/product.entity';
 import { ProductsService } from 'src/services/products/products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, Brand, Category])],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
